5e0ce920dfda9197c1a113b6e9068568157e18af
Patrick Cerny <patrick.cerny04@gmail.com>
2022-03-16 00:54:40 +0100

    preview done

--- src/pages/Preview/Preview.tsx
+++ src/pages/Preview/Preview.tsx
@@ -5,80 +5,81 @@ import {
   getDoc,
   getDocs,
   query,
-  where
-} from "firebase/firestore";
-import { getDownloadURL, ref } from "firebase/storage";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { db, storage } from "../../firebase";
-import "./Preview.scss";
+  where,
+} from 'firebase/firestore';
+import { getDownloadURL, ref } from 'firebase/storage';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import { db, storage } from '../../firebase';
+import Email from '../../images/icon-email.png';
+import Phone from '../../images/icon-phone.png';
+import Arrow from '../../images/arrow.webp';
+import './Preview.scss';
 
 const Preview: React.FC = () => {
   const { id } = useParams();
   const [item, setItem] = useState<any>([]);
-
+  const [mobile, setMobile] = useState(window.innerWidth < 820);
+  const navigate = useNavigate();
   useEffect(() => {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 820) setMobile(false);
+      else if (window.innerWidth < 820) setMobile(true);
+    });
+
     (async () => {
       let tempItems: DocumentData[] = [];
 
       (
-        await getDocs(
-          query(collection(db, "items"), where("image", "==", id))
-        )
-      ).forEach(doc => tempItems.push(doc.data()));
+        await getDocs(query(collection(db, 'items'), where('image', '==', id)))
+      ).forEach((doc) => tempItems.push(doc.data()));
 
       setItem(
         (
           await Promise.all(
-            tempItems.map(async it => ({
+            tempItems.map(async (it) => ({
               ...it,
               imageId: it.image,
-              imageUrl: await getDownloadURL(
-                ref(storage, tempItems[0].image)
-              )
+              imageUrl: await getDownloadURL(ref(storage, tempItems[0].image)),
             }))
           )
         )[0]
       );
     })();
-  }, []);
 
+    return window.removeEventListener('resize', () => {});
+  }, [id]);
+  console.log(item);
   return (
-    <div>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
-        rel="stylesheet"
-      />
-      <div className="preview_page">
-        <div className="img-title-box">
-          <div className="imageBox">
-            <img
-              src={item.imageUrl}
-              alt="item_picture"
-              className="img_responsive"
-            />
-          </div>
-          <div className="infoBox">
-            <h1>{item.title}</h1>
-            <div className="descriptionBox">
-              <p>{item.description}</p>
-            </div>
-          </div>
-          <div className="contactInfoBox">
-            <h1>Contact Information</h1>
-            <div className="contactInfoItems">
-              <div className="phoneNumber">
-                <h2>Phone Number:</h2>
-                <h3>123456789</h3>
-              </div>
-              <div className="email">
-                <h2>E-Mail:</h2>
-                <h3>yannik.derboss@xd.luschnou</h3>
-              </div>
-            </div>
-          </div>
+    <div
+      className='main_preview'
+      style={mobile ? { flexDirection: 'column', margin: ' 100px 0' } : {}}
+    >
+      <div className='main_preview__image-container'>
+        <img
+          className='main_preview__image'
+          src={item.imageUrl}
+          alt='item'
+          onClick={() => window.open(item.imageUrl, '_blank')}
+        />
+      </div>
+      <div className='main_preview__text-container'>
+        <h2 className='main_preview__text-container__title'>{item.title}</h2>
+        <p className='main_preview__text-container__description'>
+          {item.description}
+        </p>
+        <span>Color: {item.color}</span>
+        <span>Location: {item.location}</span>
+        <div className='main_preview__text-container__icons'>
+          <a href={'tel:' + item.phone}>
+            <img src={Phone} alt='Phone' />
+          </a>
+          <a href={'mailto:' + item.email}>
+            <img src={Email} alt='Email' />
+          </a>
         </div>
       </div>
+      <img id='fab' src={Arrow} alt='' onClick={() => navigate(-1)} />
     </div>
   );
 };
