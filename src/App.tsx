dcb8b4944105359b771848c81c31a199c94259b8
Patrick Cerny <patrick.cerny04@gmail.com>
2022-03-16 09:04:11 +0100

    added minor styling and 'fixed' stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
-import React from "react";
-import "./App.scss";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
-import Lost from "./pages/Lost/Lost";
-import Found from "./pages/Found/Found";
-import Navbar from "./components/NavBar/NavBar";
-import Footer from "./pages/Footer/Footer";
-import Preview from "./pages/Preview/Preview";
-import MainPage from "./pages/MainPage/MainPage";
+import React from 'react';
+import './App.scss';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Lost from './pages/Lost/Lost';
+import Found from './pages/Found/Found';
+import Navbar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import Preview from './pages/Preview/Preview';
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
-    <div id="app">
+    <div id='app'>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/lost" element={<Lost />} />
-          <Route path="/found" element={<Found />} />
-          <Route path="/items/:id" element={<Preview />} />
-          <Route path="*" element={() => <Navigate to="/" />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='/lost' element={<Lost />} />
+          <Route path='/found' element={<Found />} />
+          <Route path='/items/:id' element={<Preview />} />
+          <Route path='*' element={() => <Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
       <Footer />
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,49 @@
+import Twitter from '../../images/twitter.png';
+import Facebook from '../../images/facebook.png';
+import Instagram from '../../images/insta.png';
+import Youtube from '../../images/youtube.png';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='main_footer'>
+      <div className='main_footer__container'>
+        <span className='main_footer__container__heading'>Legal Info</span>
+        <a className='main_footer__container__item' href='/'>
+          Terms & Conditions
+        </a>
+        <a className='main_footer__container__item' href='/'>
+          Privacy Policy
+        </a>
+      </div>
+      <div className='main_footer__container'>
+        <span className='main_footer__container__heading'>Team</span>
+        <a className='main_footer__container__item' href='/'>
+          About Us
+        </a>
+        <a className='main_footer__container__item' href='/'>
+          Contact Us
+        </a>
+      </div>
+      <div className='main_footer__container'>
+        <span className='main_footer__container__heading'>Social Media</span>
+        <div className='main_footer__container__social-m'>
+          <a className='main_footer__container__item' href='/'>
+            <img src={Facebook} alt='' />
+          </a>
+          <a className='main_footer__container__item' href='/'>
+            <img src={Youtube} alt='' />
+          </a>
+          <a className='main_footer__container__item' href='/'>
+            <img src={Twitter} alt='' />
+          </a>
+          <a className='main_footer__container__item' href='/'>
+            <img src={Instagram} alt='' />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
